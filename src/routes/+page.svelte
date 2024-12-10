9220a3f6c27db749764a1ce2f315cc2f6b81faf6
str33ts4Int <shariaad@students.zhaw.ch>
2024-12-10 12:02:29 +0100

    Deploy netlify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Destination List</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<p>@students.zhaw.ch</p> 
